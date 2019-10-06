79e75b7ce35640b86567d00c3a26968e5012b9ab
Stephen Fraser <stephen.fraser@digirati.com>
2019-10-06 22:08:40 +0100

    Fixed externals

--- packages/layouts/webpack.config.js
+++ packages/layouts/webpack.config.js
@@ -3,7 +3,7 @@ const Config = require('webpack-config').default;
 module.exports = new Config().extend('@fesk/scripts/webpack').merge({
   /* Your config overrides go here */
   externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
+    react: 'react',
+    'react-dom': 'react-dom',
   },
 });
